7905437768bd0aae3a421a202fdc3cd7a4cf4421
premhowli <howli.prem@gmail.com>
2017-10-24 21:17:16 +0530

    demo commit just to check. please ignore

--- front/src/app/install/install.component.ts
+++ front/src/app/install/install.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { Router } from '@angular/router';
+import { WebService } from '../webservices/webservices.services';
 
 @Component({
   selector: 'app-install',
@@ -7,9 +10,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InstallComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: Http, private router: Router, private webservice: WebService) { }
 
-  ngOnInit() {
+  public ngOnInit() {
+    this.webservice.isAuthenticated();
   }
 
 }
